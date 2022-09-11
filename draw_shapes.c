ac73074fb59e2f46a779b2bb5584ffd657fd8f13
jlvanar <jlvanarsdall@miners.utep.edu>
2022-09-10 20:53:18 -0600

    added arrow function

--- draw_shapes.c
+++ draw_shapes.c
@@ -12,6 +12,7 @@ void print_square(int leftCol, int size)
     for (       ; col < endCol;  col++) putchar('*');
     putchar('\n');
   }
+  //end of function
 }
 
 // Prints a triangle of specified height whose left edge is at col leftCol.
@@ -24,5 +25,28 @@ void print_triangle(int leftCol, int size)
     for (       ; col <= maxCol; col++) putchar('*');
     putchar('\n');
   }
+  //end of function
 }
 
+//Prints an arrow of specified height whose left edge is at leftCol
+void print_arrow(int leftCol, int size)
+{
+  int i, j;
+  int endCol = leftCol + size;
+  //Draws a square, which forms the body of the arrow
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (col = 0; col < endCol; col++) putchar('*');
+    putchar('/n');
+  }
+  //Draws a triangle, which becomes the head
+  for(int row = 0; row <= size; row++){
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('/n');
+  }
+  //end of function 
+}
--- uimain.c
+++ uimain.c
@@ -29,6 +29,10 @@ int main()
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
+    case 'a':
+      puts("You've fired an arrooooow.");
+      print_arrow(5, 10);
+      break;      
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
